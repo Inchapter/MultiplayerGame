b8f6ce10acb6d0a05022906e5d6fde042596bc38
Inchapter <843452214@qq.com>
2019-07-01 10:42:50 +0800

    Update MultiMesh.h

--- MultiplayerTest/MultiMesh.h
+++ MultiplayerTest/MultiMesh.h
@@ -16,15 +16,16 @@ class MULTIPLAYERTEST_API AMultiMesh : public AMyStaticMeshActor
 	
 public:
 	AMultiMesh();
-
+	// replicated属性需要重写该函数
 	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty> & OutLifetimeProps) const override;
-
+	// 只有拥有网络权限的才能执行该函数
 	UFUNCTION(BlueprintAuthorityOnly, Category = "test")
 	void PickUpBy(APawn* Pawn) override;
 
 	float GetPower();
 
 protected:
+	//复制该属性，让这个类所有实例的初始值保持相同
 	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "test", Meta = (BlueprintProtected = "true"))
 	float BatteryPower;
 
